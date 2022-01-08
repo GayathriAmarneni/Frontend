5c15c788d05a004c4823482d43d581961f35766e
GayathriAmarneni <90837512+GayathriAmarneni@users.noreply.github.com>
2022-01-08 18:56:37 +0530

    Update articleEditComponent.js

--- src/articleEditComponent.js
+++ src/articleEditComponent.js
@@ -1,25 +1,23 @@
 import React, { useState, useEffect } from 'react';
 
 function ArticleEdit(props) {
-    const [lecture, setLecture] = useState(props.object);
-    useEffect(() => {
-        if(lecture.article.state === 'DISPLAY') {
-            props.onSave(lecture);
-        }
-    });
+   
+    const[article, updateArticle] = useState(props.articleObject);
+    
     const handleOnChange = (event) => {
-        const updatedLec = {...lecture,...{article:{content: event.target.value, state: 'EDIT'}}}
-        setLecture(updatedLec);
+        const articleObj = {...article,...{content: event.target.value}};
+        updateArticle(articleObj);
     }
 
     const handleOnSave = (event) => {
-        const updatedLec = {...lecture,...{article: {content: lecture.article.content, state: 'DISPLAY'}}}
-        setLecture(updatedLec);
+        const updatedArticle = {...article,... {state: 'DISPLAY'}}
+        updateArticle(updatedArticle);
+        props.onSave(updatedArticle);
     }
-    
+
     return(
         <div id = "textAreaDivId" >
-            <textarea id = "textAreaId" rows = "3" cols = "70" placeholder = "Enter text" value = {lecture.article.content} onChange = {handleOnChange} ></textarea>
+            <textarea id = "textAreaId" rows = "3" cols = "70" placeholder = "Enter text" value = {article.content} onChange = {handleOnChange} ></textarea>
             <button className = "saveTextArea" onClick = {handleOnSave}>Save</button>
             <button className = "classRemoveContent" onClick = {props.cancelTextareaDiv} >
             <span className="glyphicon glyphicon-remove"></span>
