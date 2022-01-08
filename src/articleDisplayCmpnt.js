b4f33ecd54a5591643e2ae70993dbdc69771ae01
GayathriAmarneni <90837512+GayathriAmarneni@users.noreply.github.com>
2022-01-08 18:55:47 +0530

    Update articleDisplayCmpnt.js

--- src/articleDisplayCmpnt.js
+++ src/articleDisplayCmpnt.js
@@ -3,7 +3,7 @@ import React from 'react';
 function ArticleDisplay(props) {
     return(
         <div id = "articleLabelId" >
-            <label>Article: {props.object.article.content} </label>
+            <label>Article: {props.content} </label>
             <button className = "articleEdit" id = "articleEditBtnId" onClick = {props.edit} >Edit</button>
             <button className = "classRemove" onClick = {props.cancel} >
             <span className="glyphicon glyphicon-remove"></span>
